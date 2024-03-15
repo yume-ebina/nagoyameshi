941e6a6bd013f03513edb2c8c7e734ed36e87a4e
yume-ebina <cityeventa@gmail.com>
2024-03-15 17:15:19 +0900

    deviseでuser作成

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,17 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
+  devise_for :users, :controllers => {
+    :registrations => 'users/registrations',
+    :sessions => 'users/sessions',
+    :passwords => 'users/passwords',
+    :confirmations => 'users/confirmations',
+    :unlocks => 'users/unlocks',
+  }
 
-  # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  devise_scope :user do
+    root :to => "users/sessions#new"
+    get "signup", :to => "users/registrations#new"
+    get "verify", :to => "users/registrations#verify"
+    get "login", :to => "users/sessions#new"
+    delete "logout", :to => "users/sessions#destroy"
+  end
 end
