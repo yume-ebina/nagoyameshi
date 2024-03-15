7ca0209a8662810d168d037cff28f7b141019a69
yume-ebina <cityeventa@gmail.com>
2024-03-15 17:16:56 +0900

    ルート設定

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  devise_for :admins, :controllers => {
+    :sessions => 'admins/sessions'
+  }
+
+  devise_scope :admin do
+    get "dashboard", :to => "dashboard#index"
+    get "dashboard/login", :to => "admins/sessions#new"
+    post "dashboard/login", :to => "admins/sessions#create"
+    delete "dashboard/logout", :to => "admins/sessions#destroy"
+  end
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
     :sessions => 'users/sessions',
