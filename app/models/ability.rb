a9e19c9b19a131f3c5fccf6e16990976c37fdebe
yume-ebina <cityeventa@gmail.com>
2024-03-21 14:41:19 +0900

    style:インデント調節_2

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -1,10 +1,10 @@
 class Ability
-	include CanCan::Ability
+  include CanCan::Ability
 
-	def initialize(user)
-		if user && user.admin?
-			can :access, :rails_admin
-			can :manage, :all
-		end
-	end
+  def initialize(user)
+    if user && user.admin?
+    can :access, :rails_admin
+    can :manage, :all
+    end
+  end
 end
