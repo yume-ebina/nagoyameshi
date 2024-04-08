20079f5731cc3fb04fd2cdf3521f20e6bdbd09ad
yume-ebina <cityeventa@gmail.com>
2024-04-08 14:57:57 +0900

    feat:ユーザーが店舗をお気に入りに追加する機能を実装しました

---
+++ app/controllers/likes_controller.rb
@@ -0,0 +1,24 @@
+class LikesController < ApplicationController
+  before_action :authenticate_user!
+  before_action :set_restaurant
+
+  # POST /restaurants/:restaurant_id/like
+  def create
+    @like = current_user.likes.create(restaurant_id: params[:restaurant_id])
+    redirect_to @restaurant
+  end
+
+  # DELETE /restaurants/:restaurant_id/unlike
+  def destroy
+    # お気に入り登録を解除
+    @like = current_user.likes.find_by(restaurant_id: @restaurant.id)
+    @like.destroy
+    redirect_to @restaurant
+  end
+
+  private
+  def set_restaurant
+    @restaurant = Restaurant.find(params[:restaurant_id])
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_04_03_071743) do
+ActiveRecord::Schema[7.1].define(version: 2024_04_07_232238) do
   create_table "admins", force: :cascade do |t|
     t.string "corporate_name", null: false
     t.string "email", default: "", null: false
@@ -28,6 +28,15 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_03_071743) do
     t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+  end
+
+  create_table "likes", force: :cascade do |t|
+    t.integer "user_id", null: false
+    t.integer "restaurant_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["restaurant_id"], name: "index_likes_on_restaurant_id"
+    t.index ["user_id"], name: "index_likes_on_user_id"
   end
 
   create_table "restaurants", force: :cascade do |t|
@@ -79,6 +88,8 @@ ActiveRecord::Schema[7.1].define(version: 2024_04_03_071743) do
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "likes", "restaurants"
+  add_foreign_key "likes", "users"
   add_foreign_key "restaurants", "categories"
   add_foreign_key "reviews", "restaurants"
   add_foreign_key "reviews", "users"
