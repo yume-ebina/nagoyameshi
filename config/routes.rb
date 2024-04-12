02fce82c9b2e8ae142e1f65514f93ed5ab827108
yume-ebina <cityeventa@gmail.com>
2024-04-12 13:55:27 +0900

    余白削除

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,6 @@
 Rails.application.routes.draw do
   mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
 
-  
-
   resource :users, only: [:edit, :update] do
     collection do
       get "mypage", :to => "users#mypage"
