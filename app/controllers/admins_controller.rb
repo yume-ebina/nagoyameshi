fc0c2e4efa5b4521224d3e1b7a87f6695364f7c8
yume-ebina <cityeventa@gmail.com>
2024-04-15 19:54:23 +0900

    集計機能リファクタリング

--- app/controllers/admins_controller.rb
+++ app/controllers/admins_controller.rb
@@ -7,6 +7,6 @@ class AdminsController < ApplicationController
     @premium_users = User.where.not(premium_end_date: nil).count
 
     #有効なプレミアムユーザー
-    @effective_premium_user = User.where(premium_end_date: Time.now...).count
+    @effective_premium_user = User.where(premium_end_date: Time.current...).count
   end
 end
