09aecc7c76aed39723339fdd6b5fe9b7cdaec6a0
yume-ebina <cityeventa@gmail.com>
2024-04-19 10:43:30 +0900

    不要なコメント削除

--- app/models/ability.rb
+++ app/models/ability.rb
@@ -8,32 +8,15 @@ class Ability
       can :access, :rails_admin
       can :manage, :all
 
-    # プレミアムユーザーはモデルの操作を行うことができるが、管理者画面は閲覧不可能
+    # プレミアムユーザーは管理者画面の閲覧のみ不可能
     elsif user.premium_end_date.present? && user.premium_end_date > Time.zone.today
       can :manage, :all
       cannot :access, :rails_admin
       cannot :aggregate, :AdminsController
 
-    # 閲覧のみ可能、管理者画面は閲覧不可能
+    # 一般ユーザーはCRUD操作を制限し、管理者画面は閲覧不可能
     else
       can :read, :all
     end
-
-  # can :manage, [:session, :registration]
-  # 管理者はモデルの操作を行うことができて管理者画面を閲覧可能
-  # if user.admin?
-  #   can :access, :rails_admin
-  #   can :manage, :all
-
-  # プレミアムユーザーはモデルの操作を行うことができるが、管理者画面は閲覧不可能
-  # elsif user.premium_end_date.present? && user.premium_end_date > Time.zone.today
-  #   can :manage, :all
-  #   cannot :access, :rails_admin
-
-  # role が read_only のユーザーはモデルの操作を行うことができず閲覧のみ可能、管理者画面は閲覧不可能
-  # else
-  #   can :read, :all
-  #   cannot :access_admin_page, :all
-  # end
   end
 end
